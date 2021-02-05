ee81d175b11bea95af43789c54050c5b438bb028
Makavura <makavuramughanga@gmail.com>
2021-02-05 18:02:16 +0300

    a game of darts

--- darts.js
+++ darts.js
@@ -1,5 +1,3 @@
-
-
 /* 
 
 Dart
@@ -8,24 +6,31 @@ Dart
 Boundaries = -10, -10, 10, 10
 */
 
-export default function score(x, y) {
+function score(x, y) {
 
     let innerradius = 1, // 10 Points
         middleradius = 5, //  5 Points.
         outerradisu = 10, // 1 Point.  > 10 --> loser
         finalscore = 0;
 
-    if (x > 10 || y > 10 || x < -10 || y < -10) { // outside target
+    if (x > 10 || y > 10 ) { // outside target
         finalscore = 0;
     } else { //within target
         finalscore += 0;
-        if (x >= 5 && x < 10 || y >= 5 && y < 10 || x <= -5 && x > 10 || y <= -5 && y > 10) {  // outermost
+        if (x >= 5 && x < 10 && y >= 5 && y < 10) {  // outermost
             finalscore += 1;
-        } else if (x >= 1 && x < 5 || y >= 1 && y < 5 || x > -5 && x <= -1 || y > -5 && y <= -1) { // middlemost
+        } else if (x >= 1 && x < 5 && y >= 1 && y < 5) { // middlemost
             finalscore += 5;
-        } else if (x < 1 && y < 1 || x > -1 && y > -1) { // innermost
+        } else if (x < 1 && y < 1 ) { // innermost
             finalscore += 10;
         }
     }
 
+    console.log(finalscore);
 }
+
+score(0, 10);
+score(5, 5);
+score(4, 2);
+score(0,0);
+score(1,1);
