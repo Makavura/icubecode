c0bb8e0a2781fb273b2a7144ba1124acadf462d2
Makavura <makavuramughanga@gmail.com>
2021-02-05 18:52:15 +0300

    docs

--- iou.tracker.js
+++ iou.tracker.js
@@ -11,9 +11,39 @@ const port = 3000;
 const chalk = require('chalk');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Welcome to IOU Tracker API V1 By Makavura Mughanga')
+});
+
+app.post('/users', (req, res) => {
+    /* 
+    Given a list of users {"users":["Adam","Bob"]} 
+    Return a response of users matching those names sorted by name
+    */
+});
+
+app.post('/add', (req, res) => {
+    /* 
+    Given a name e.g {"user": "Dan"}
+    Create a user
+    Return a User Object for a new user
+    */
+});
+
+app.post('/iou', (req, res) => {
+    /* 
+    Given 
+    {"lender": "Dan","borrower": "Amos","amount":5.25}
+    Return  a response of users affected by IOU entry sorted by name
+    */
+});
+
+app.delete('/user', (req, res) => {
+    /* 
+    Given a user's name
+    Delete all IOUs related to name
+    */
+});
 
 app.listen(port, () => {
     console.log(chalk.yellowBright(`Example app listening at http://localhost:${port}`));
-})
+});
